ed1555a65a7ceb736d083684d1e78a787b6f2004
Facundo Farias <contacto@facundofarias.com.ar>
2016-03-29 06:48:46 -0300

    Initial commit

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  summary: "Smart package for Meteor that adds filter and pager behavior to our Meteor's collections."
+});
+
+Package.on_use(function(api) {
+  api.use('underscore', ['client', 'server']);
+
+  api.add_files(['filter-collections-client.js'], ['client']);
+  api.add_files(['filter-collections-server.js'], ['server']);
+});
